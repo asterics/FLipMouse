9c6b76a9621d3a806270cfd537eed88311f2d47b
Chris Veigl <veigl@technikum-wien.at>
2016-01-26 20:54:39 +0100

    fixed alignment bug for Cortex M0 / TeensyLC

--- FLipWare/cim.cpp
+++ FLipWare/cim.cpp
@@ -202,26 +202,31 @@ uint8_t process_ARE_frame(uint8_t status_code)
 void reply_FeatureList(void)
 {
   
-	Serial.write ( (uint8_t *)&CIM_frame, CIM_HEADER_LEN);
+  Serial.write ((uint8_t *) &CIM_frame, 7);    // attention : byte alignment in struct not possible for Cortex M0 !
+  Serial.write ((uint8_t *) &(CIM_frame.cim_feature), 4);
 	Serial.write  ( (uint8_t *)&LIPMOUSE_CIM_FEATURELIST, CIM_frame.data_size);
 }
 
 void reply_UniqueNumber(void)
 {
 	CIM_frame.data_size=4;    // lenght of unique number
-	Serial.write ((uint8_t *) &CIM_frame, CIM_HEADER_LEN);
+  Serial.write ((uint8_t *) &CIM_frame, 7);    // attention : byte alignment in struct not possible for Cortex M0 !
+  Serial.write ((uint8_t *) &(CIM_frame.cim_feature), 4);
 	Serial.write ((uint8_t *) &LIPMOUSE_CIM_UNIQUE_NUMBER, CIM_frame.data_size);
 }
 
 void reply_Acknowledge(void)
 {
-        CIM_frame.data_size=0;     // no data in ack frame
-	Serial.write ((uint8_t *) &CIM_frame, CIM_HEADER_LEN);
+  CIM_frame.data_size=0;     // no data in ack frame
+  Serial.write ((uint8_t *) &CIM_frame, 7);    // attention : byte alignment in struct not possible for Cortex M0 !
+  Serial.write ((uint8_t *) &(CIM_frame.cim_feature), 4);
 }
 
 void reply_DataFrame(void)
 {
-	Serial.write ((uint8_t *) &CIM_frame, CIM_HEADER_LEN);
+	Serial.write ((uint8_t *) &CIM_frame, 7);    // attention : byte alignment in struct not possible for Cortex M0 !
+  Serial.write ((uint8_t *) &(CIM_frame.cim_feature), 4);
+  
 	Serial.write ((uint8_t *) CIM_frame.data, CIM_frame.data_size);
 }
 

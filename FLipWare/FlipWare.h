06eb9f9c963edb80312a13719e4d4c78823af552
Benjamin Klaus <klaus@technikum-wien.at>
2021-09-08 13:13:52 +0200

    updated version number

--- FLipWare/FlipWare.h
+++ FLipWare/FlipWare.h
@@ -48,7 +48,7 @@
 #include "bluetooth.h"
 #include "hid_hal.h"
 
-#define VERSION_STRING "Flipmouse v2.11"
+#define VERSION_STRING "Flipmouse v2.11.1"
 
 //  V2.11: eeprom access optimization and support for deletion / update of individual slots
 //  V2.10: code size reduction: using floating point math, removed debug level control via AT E0, AT E1 and AT E2

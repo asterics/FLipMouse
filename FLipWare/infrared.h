49c2c806b09de2a7f1e0717fee0149ea5b07db56
beni <aignerb@technikum-wien.at>
2023-09-06 15:25:08 +0200

    cherry-picked some IR stuff from FABI3

--- FLipWare/infrared.h
+++ FLipWare/infrared.h
@@ -43,6 +43,12 @@
 // maximum time interval which can be stored in high precision (microseconds) format
 #define MAX_HIGHPRECISION_DURATION 64000
 
+// PWM value for TX (max. 8bit). Warning: be careful not to overdrive anything!
+#define IR_PWM_ON 170
+
+//if activated, firmware will create a new alarm pool. If 0, default alarm pool will be used
+#define IR_ALARM_POOL 0
+
 
 /**
    @name initIR
